e5fcefc8b8b902eb05452c2a9f68eafb7a2ba1cd
disco07 <koneenok@outlook.fr>
2022-08-22 07:12:31 +0200

    first commit

--- main.go
+++ main.go
@@ -1,7 +1,26 @@
 package main
 
-import "net/http"
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
 
 type app struct {
 	client http.Client
+}
+
+func main() {
+	//url := os.Args[1]
+	//urlSplit := strings.Split(url, "/")
+	//filename := urlSplit[len(urlSplit)-1]
+	res, err := http.Head("https://agritrop.cirad.fr/584726/1/Rapport.pdf")
+	if err != nil {
+		log.Fatal(err)
+		return
+	}
+	fmt.Println(res.Header.Get("Accept-Ranges"))
+	if res.Header.Get("Accept-Ranges") == "bytes" {
+
+	}
 }
