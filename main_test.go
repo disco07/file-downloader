ef12ff8905ce7a87b150d3436a1ba798bc34145c
disco07 <koneenok@outlook.fr>
2022-09-14 23:52:31 +0200

    test

--- main_test.go
+++ main_test.go
@@ -6,26 +6,45 @@ import (
 	"testing"
 )
 
-var downloaderTests = []struct {
-	url      string
-	expected error
-}{
-	{"https://agritrop.cirad.fr/584726/1/Rapport.pdf", nil},
-	{"", fmt.Errorf("invalid url")},
-	{"https://youtu.be/w0NQlEMjntI", errors.New("unable to download file with multithreads")},
-	{"https://github.com/disco07/file-downloader", errors.New("unable to parse variable")},
-}
-
 func TestDownloader(t *testing.T) {
-	for _, tt := range downloaderTests {
-		err := worker(tt.url)
-		if tt.expected == nil && err != nil {
-			t.Errorf("Unexpected error for input %v: %v (expected %v)", tt.url, err, tt.expected)
-		}
+	var downloaderTests = []struct {
+		description string
+		url         string
+		expected    error
+	}{
+		{
+			description: "Download work",
+			url:         "https://agritrop.cirad.fr/584726/1/Rapport.pdf",
+			expected:    nil,
+		},
+		{
+			description: "invalid url",
+			url:         "",
+			expected:    fmt.Errorf("invalid url"),
+		},
+		{
+			description: "unable to download file with multithreading",
+			url:         "https://youtu.be/w0NQlEMjntI",
+			expected:    errors.New("unable to download file with multithreads"),
+		},
+		{
+			description: "unable to parse variable",
+			url:         "https://github.com/disco07/file-downloader",
+			expected:    errors.New("unable to parse variable"),
+		},
+	}
 
-		if tt.expected != nil && err.Error() != tt.expected.Error() {
-			t.Errorf("Unexpected error for input %v: %v (expected %v)", tt.url, err, tt.expected)
-		}
+	for _, tt := range downloaderTests {
+		t.Run(tt.description, func(t *testing.T) {
+			err := worker(tt.url)
+			if tt.expected == nil && err != nil {
+				t.Errorf("Unexpected error for input %v: %v (expected %v)", tt.url, err, tt.expected)
+			}
+
+			if tt.expected != nil && err.Error() != tt.expected.Error() {
+				t.Errorf("Unexpected error for input %v: %v (expected %v)", tt.url, err, tt.expected)
+			}
+		})
 	}
 }
 
